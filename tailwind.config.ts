49453f2e72e4468212e1e5a1983675f152cce9d7
Aanagha <anaghakherde@gmail.com>
2024-08-18 21:05:05 +0530

    'functionalities'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
 import { fontFamily } from "tailwindcss/defaultTheme";
 import svgToDataUri from "mini-svg-data-uri";
- import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette';
+
 
 const config = {
   darkMode: ["class"],
@@ -80,8 +80,8 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"),addVariablesForColors,
-    function ({ matchUtilities, theme }: any) {
+  plugins: [require("tailwindcss-animate"),
+    function ({ matchUtilities }: any) {
       matchUtilities(
         {
           "bg-grid": (value: any) => ({
@@ -99,19 +99,9 @@ const config = {
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
             )}")`,
           }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+        }
       );
     },],
 } satisfies Config
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
- 
-  addBase({
-    ":root": newVars,
-  });
-}
+
 export default config
