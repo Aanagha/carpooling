7467fb9e292cac3ada0e7b21dd3044f2e0d5b22a
Aanagha <anaghakherde@gmail.com>
2024-08-25 15:43:45 +0530

    func

--- app/components/Register.tsx
+++ app/components/Register.tsx
@@ -166,7 +166,7 @@ const Register: React.FC = () => {
             <Button
               type="submit"
               variant={"outline"}
-              className="hover:bg-blue-700 bg-blue-500 text-gray-200 font-bold py-2 px-4 rounded w-full"
+              className="hover:bg-blue-700 bg-blue-500 text-white font-bold py-2 px-4 rounded w-full"
             >
               {loading ? (
                 <div
